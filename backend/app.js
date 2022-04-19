54fcd6f880546caa51031a25e4d44a7da0b58316
Alfaaz06 <abhaybhaskar2001@gmail.com>
2022-04-19 21:23:31 +0530

    Initial Commit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: 'backend/config/config.env' })
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const data = require('./routes/data');
+app.use('/api/v1', data);
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname + '../frontend/build/index.html'));
+});
+
+module.exports = app;
