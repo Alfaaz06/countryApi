cd1071db2f23d26012ad153a2dae73f675b81a61
Abhay Bhaskar <64357414+Alfaaz06@users.noreply.github.com>
2022-07-08 02:16:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use('/api/v1', data);
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname + '../frontend/build/index.html'));
 });
 
